0d625952a4814c926c8b64f63f6f771359b4298a
Daniel Alves Farias <daniel_h.m@outlook.com>
2022-06-10 12:06:48 -0300

    Add foreground image and the foreground animation

---
+++ fg.py
@@ -0,0 +1,16 @@
+import pygame as pg
+
+
+class Fg:
+    def __init__(self, pos):
+        self.foreground = pg.image.load('./assets/base.png')
+        self.speed = 2
+        self.pos = pos
+
+    def move(self):
+        self.pos[0] -= self.speed
+
+        if self.pos[0] < -288:
+            self.pos[0] = 288  + self.speed
+
+        return self.pos
--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from bg import Bg
+from fg import Fg
 
 
 class Game:
@@ -17,6 +18,9 @@ class Game:
         self.bg1 = Bg([0, 0])
         self.bg2 = Bg([self.width, 0])
 
+        self.fg1 = Fg([0, 400])
+        self.fg2 = Fg([self.width, 400])
+
 
     def loop(self):
 
@@ -30,6 +34,9 @@ class Game:
                     
             self.screen.blit(self.bg1.background, self.bg1.move())
             self.screen.blit(self.bg2.background, self.bg2.move())
+
+            self.screen.blit(self.fg1.foreground, self.fg1.move())
+            self.screen.blit(self.fg2.foreground, self.fg2.move())
 
             pygame.display.update()
 
